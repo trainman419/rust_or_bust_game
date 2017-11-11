300ccae4b0e9f3b832d0facf5627d0065a6243eb
Christopher Patton <chpatton013@gmail.com>
2017-11-10 23:24:58 -0800

    Fix swallowed events; ty daniel

--- src/handler.rs
+++ src/handler.rs
@@ -121,61 +121,61 @@ pub trait EventHandler: InputHandler + UpdateHandler + WindowHandler {
   ) -> error::Result<()> {
     // Dispatch input events to InputHandler functions.
     if let Some(button) = event.button_args() {
-      return self.on_button::<Event>(&event, &button);
+      self.on_button::<Event>(&event, &button)?;
     }
     if let Some(controller_axis) = event.controller_axis_args() {
-      return self.on_controller_axis::<Event>(&event, &controller_axis);
+      self.on_controller_axis::<Event>(&event, &controller_axis)?;
     }
     if let Some(mouse_cursor) = event.mouse_cursor_args() {
-      return self.on_mouse_cursor::<Event>(&event, &mouse_cursor);
+      self.on_mouse_cursor::<Event>(&event, &mouse_cursor)?;
     }
     if let Some(mouse_relative) = event.mouse_relative_args() {
-      return self.on_mouse_relative::<Event>(&event, &mouse_relative);
+      self.on_mouse_relative::<Event>(&event, &mouse_relative)?;
     }
     if let Some(mouse_scroll) = event.mouse_scroll_args() {
-      return self.on_mouse_scroll::<Event>(&event, &mouse_scroll);
+      self.on_mouse_scroll::<Event>(&event, &mouse_scroll)?;
     }
     if let Some(press) = event.press_args() {
-      return self.on_press::<Event>(&event, &press);
+      self.on_press::<Event>(&event, &press)?;
     }
     if let Some(release) = event.release_args() {
-      return self.on_release::<Event>(&event, &release);
+      self.on_release::<Event>(&event, &release)?;
     }
     if let Some(text) = event.text_args() {
-      return self.on_text::<Event>(&event, &text);
+      self.on_text::<Event>(&event, &text)?;
     }
     if let Some(touch) = event.touch_args() {
-      return self.on_touch::<Event>(&event, &touch);
+      self.on_touch::<Event>(&event, &touch)?;
     }
 
     // Dispatch update events to UpdateHandler functions.
     if let Some(idle) = event.idle_args() {
-      return self.on_idle::<Event>(&event, &idle);
+      self.on_idle::<Event>(&event, &idle)?;
     }
     if let Some(update) = event.update_args() {
-      return self.on_update::<Event>(&event, &update);
+      self.on_update::<Event>(&event, &update)?;
     }
 
     // Dispatch window events to WindowHandler functions.
     if let Some(after_render) = event.after_render_args() {
-      return self.on_after_render::<Event>(&event, &after_render);
+      self.on_after_render::<Event>(&event, &after_render)?;
     }
     if let Some(close) = event.close_args() {
-      return self.on_close::<Event>(&event, &close);
+      self.on_close::<Event>(&event, &close)?;
     }
     if let Some(cursor) = event.cursor_args() {
-      return self.on_cursor::<Event>(&event, cursor);
+      self.on_cursor::<Event>(&event, cursor)?;
     }
     if let Some(focus) = event.focus_args() {
-      return self.on_focus::<Event>(&event, focus);
+      self.on_focus::<Event>(&event, focus)?;
     }
     if let Some(render) = event.render_args() {
-      return self.on_render::<Event>(&event, &render);
+      self.on_render::<Event>(&event, &render)?;
     }
     if let Some(resize) = event.resize_args() {
-      return self.on_resize::<Event>(&event, &resize);
+      self.on_resize::<Event>(&event, &resize)?;
     }
 
-    Err(error::Error::from("Failed to discover event type"))
+    Ok(())
   }
 }
